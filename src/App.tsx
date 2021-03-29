a4ea6e931239b71f9a5c651efdcd96fa3e71b20b
Bon-eng <keitaymzk@gmail.com>
2021-03-29 19:46:55 +0900

    FunctionComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 interface AppProps {
-  message: string;
+  message?: string;
 }
 
-const App = ({ message }: AppProps) => {
+//Componentにはデフォルト値がある
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
   return <div>{message}</div>;
 };
+
+App.defaultProps = {
+  message: 'Hello, defaultProps!',
+}
 
 export default App;
